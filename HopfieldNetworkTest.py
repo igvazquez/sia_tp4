b6637ea3b9db2ed824d5880516be7b1da0939107
MatiMonaco <matimonaco@outlook.com>
2020-10-06 13:13:44 -0300

    Hopfield

---
+++ HopfieldNetworkTest.py
@@ -0,0 +1,65 @@
+import numpy as np
+from HopfieldNetwork import HopfieldNetwork
+import json
+
+
+def printLetter(pattern):
+    for i in range(5):
+        s = ""
+        for j in range(5):
+            val = pattern[i * 5 + j]
+            s += "# " if val == 1 else ". "
+        print(s)
+
+
+def randomizePatterns(to_randomize, randomProb):
+    out = []
+    for i in range(len(to_randomize)):
+        pattern = np.copy(patterns[i])
+        rand = np.random.rand(len(pattern))
+        for j in range(len(pattern)):
+            pattern[j] = pattern[j] * -1 if rand[j] <= randomProb else pattern[j]
+        out = np.append(out, pattern, axis=0)
+    out = np.reshape(out, (len(to_randomize), len(to_randomize[0])))
+    return out
+
+
+def getPatternsFromFile(file):
+    s = file.read()
+    ps = []
+    letter_count = 0
+    letter_length = 0
+
+    for j in range(len(s)):
+        c = s[j]
+        if c != '\n' and c != ',' and c != ' ':
+            ps = np.append(ps, 1 if c == '#' else -1)
+            if letter_count == 0:
+                letter_length += 1
+        elif c == ',':
+            letter_count += 1
+
+    return np.reshape(ps, (letter_count, letter_length))
+
+
+with open('./data/config.json') as json_file:
+    data = json.load(json_file)
+    for p in data['ej2']:
+        print('Patterns file: '+ p['patterns_file'])
+        print('Random Probability: ' + p['random_prob'])
+        print('Epsilon: ' + p['epsilon'])
+        print('Maximum epochs: ' + p['max_epochs'])
+        print('')
+patterns_file_name = p['patterns_file']
+randomProbability = float(p['random_prob'])
+epsilon = float(p['epsilon'])
+max_epochs = int(p['max_epochs'])
+patterns_file = open("data/"+patterns_file_name, "r+")
+
+patterns = getPatternsFromFile(patterns_file)
+
+
+
+random_patterns = randomizePatterns(patterns, randomProbability)
+hpn = HopfieldNetwork(patterns)
+outputs = hpn.classify(random_patterns, max_epochs, epsilon)
