640627dfd10416168938e74d8fbb8ee17da6796c
MatiMonaco <matimonaco@outlook.com>
2020-10-20 11:22:17 -0300

    graphics

--- OjasRuleTest.py
+++ OjasRuleTest.py
@@ -1,8 +1,10 @@
 from OjasRuleNeuron import OjasRuleNeuron
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+import seaborn as sns
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import json
 
 with open('./data/config.json') as json_file:
@@ -24,5 +26,22 @@ ORN = OjasRuleNeuron()
 FPC,history = ORN.fit(learn_factor,std_df,max_epochs)
 
 pca = PCA(n_components=7)
-components = pca.fit_transform(std_df)
+lib_components = pca.fit_transform(std_df)
 print('Library first principal component:\n',pca.components_[0])
+data_components = std_df.dot(FPC)
+df_2 = pd.DataFrame({'Pais': df['Country'], 'PCA1': data_components})
+plt.figure(figsize=(7, 5))
+plt.xticks(rotation=90)
+plt.title("Using Oja's Rule First Principal Component")
+sorted = df_2.sort_values(by='PCA1', ascending=True)
+sns.barplot(x='Pais', y='PCA1', data=sorted)
+
+
+df_3 = pd.DataFrame({'Pais': df['Country'], 'PCA1': lib_components[:,0]})
+plt.figure(figsize=(7, 5))
+plt.xticks(rotation=90)
+plt.title("Using Library's First Principal Component")
+sorted = df_3.sort_values(by='PCA1', ascending=True)
+sns.barplot(x='Pais', y='PCA1', data=sorted)
+
+plt.show()
