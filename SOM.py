9816c50f26c75f2eb318ec01fef49c9ef95493c0
Ignacio Vazquez <40323334+igvazquez@users.noreply.github.com>
2020-10-21 01:52:46 -0300

    update

--- SOM.py
+++ SOM.py
@@ -19,7 +19,7 @@ class SOM(object):
         self._sigma = sigma
         self._input_len = input_len
 
-        self._weights = np.random.rand(x, y, input_len) * 2 - 1
+        self.weights = np.random.rand(x, y, input_len) * 2 - 1
 
         self._activation_map = np.zeros((x, y))
         self._neigx = np.arange(x)
@@ -31,8 +31,10 @@ class SOM(object):
 
         self._update_function = update_function
 
+
+
     def _activate(self, x):
-        self._activation_map = self._distance_function(x, self._weights)
+        self._activation_map = self._distance_function(x, self.weights)
 
     def _distance_function(self, x, w):
         return np.linalg.norm(np.subtract(x, w), axis=-1)
@@ -52,13 +54,10 @@ class SOM(object):
         eta = self._update_function(self._learning_rate, t, max_iteration)
         sigma = self._update_function(self._sigma, t, max_iteration)
         h = self.neighborhood(w, sigma)
-        self._weights += np.einsum('ij, ijk->ijk', eta*h, x-self._weights)
+        self.weights += np.einsum('ij, ijk->ijk', eta * h, x - self.weights)
         # ij, ijk son los labels de las dos matrices eta*h es 2D y x-W es 3D
 
-
-
     def train(self, training_data, max_iterations):
-
         for epoch in range(max_iterations):
             for i in range(len(training_data)):
                 self.update(training_data[i], self.winner_neuron(training_data[i]), i, max_iterations)
@@ -74,75 +73,35 @@ class SOM(object):
         return winmap
 
     def distance_map(self):
-        um = np.zeros((self._weights.shape[0], self._weights.shape[1], 8))
+        u = np.zeros((self.weights.shape[0], self.weights.shape[1], 8))
 
         ii = [[0, -1, -1, -1, 0, 1, 1, 1]] * 2
         jj = [[-1, -1, 0, 1, 1, 1, 0, -1]] * 2
 
-        for x in range(self._weights.shape[0]):
-            for y in range(self._weights.shape[1]):
-                w_2 = self._weights[x, y]
+        for x in range(self.weights.shape[0]):
+            for y in range(self.weights.shape[1]):
+                w_2 = self.weights[x, y]
                 e = y % 2 == 0  # only used on hexagonal topology
                 for k, (i, j) in enumerate(zip(ii[e], jj[e])):
-                    if x + i >= 0 and x + i < self._weights.shape[0] and y + j >= 0 and y + j < self._weights.shape[1]:
-                        w_1 = self._weights[x + i, y + j]
-                        um[x, y, k] = np.linalg.norm(w_2 - w_1)
+                    if x + i >= 0 and x + i < self.weights.shape[0] and y + j >= 0 and y + j < self.weights.shape[1]:
+                        w_1 = self.weights[x + i, y + j]
+                        u[x, y, k] = np.linalg.norm(w_2 - w_1)
 
-        um = um.sum(axis=2)
-        return um / um.max()
+        u = u.sum(axis=2)
+        return u / u.max()
 
 
 df = pd.read_csv('europe.csv')
 X_cols = ['Area', 'GDP', 'Inflation', 'Life.expect', 'Military', 'Pop.growth', 'Unemployment']
-# print(data[X_cols].to_numpy())
+
 som = SOM(6, 6, 7, 3)
-# inp = np.array([0.2, 0.2])
+
 data = df[X_cols]
 data = (data - np.mean(data, axis=0)) / np.std(data, axis=0)
 data = data.values
 
 som.train(data, 1000)
 winner_coordinates = np.array([som.winner_neuron(x) for x in data]).T
-
-print(winner_coordinates[0][25])
-print(winner_coordinates[1][25])
-
-print(winner_coordinates[0][16])
-print(winner_coordinates[1][16])
-
-print(winner_coordinates[0][18])
-print(winner_coordinates[1][18])
-
-country_codes = {
-    'Austria': 'AT',
-    'Belgium': 'BE',
-    'Bulgaria': 'BG',
-    'Croatia': 'HR',
-    'Czech Republic': 'CZ',
-    'Denmark': 'DK',
-    'Estonia': 'EE',
-    'Finland': 'FI',
-    'Germany': 'DE',
-    'Greece': 'GR',
-    'Hungary': 'HU',
-    'Iceland': 'IS',
-    'Ireland': 'IE',
-    'Italy': 'IT',
-    'Latvia': 'LV',
-    'Lithuania': 'LT',
-    'Luxembourg': 'LU',
-    'Netherlands': 'NL',
-    'Norway': 'NO',
-    'Poland': 'PL',
-    'Portugal': 'PT',
-    'Slovakia': 'SK',
-    'Slovenia': 'SI',
-    'Spain': 'ES',
-    'Sweden': 'SE',
-    'Switzerland': 'CH',
-    'Ukraine': 'UA',
-    'United Kingdom': 'GB'}
-
 
 country_map = som.labels_map(data, df['Country'])
 
@@ -161,7 +120,7 @@ plt.grid()
 plt.show()
 
 
-W = som._weights
+W = som.weights
 plt.figure(figsize=(14, 14))
 for i, f in enumerate(X_cols):
     plt.subplot(3, 3, i+1)
@@ -174,8 +133,8 @@ plt.show()
 
 Z = np.zeros((6, 6))
 plt.figure(figsize=(8, 8))
-for i in np.arange(som._weights.shape[0]):
-    for j in np.arange(som._weights.shape[1]):
+for i in np.arange(som.weights.shape[0]):
+    for j in np.arange(som.weights.shape[1]):
         feature = np.argmax(W[i, j, :])
         plt.plot([j + .5], [i + .5], 'o', color='C' + str(feature),
                  marker='s', markersize=24)
