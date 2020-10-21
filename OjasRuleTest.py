5117218d0828294dbad09ab16d02fee9fc73bd0d
MatiMonaco <matimonaco@outlook.com>
2020-10-21 16:26:45 -0300

    update

--- OjasRuleTest.py
+++ OjasRuleTest.py
@@ -23,10 +23,11 @@ X_cols = ['Area', 'GDP', 'Inflation', 'Life.expect', 'Military', 'Pop.growth', '
 std_df = StandardScaler().fit_transform(df[X_cols])
 
 ORN = OjasRuleNeuron()
-FPC,history = ORN.fit(learn_factor,std_df,max_epochs)
-
+FPC,epochs,history = ORN.fit(learn_factor,std_df,max_epochs)
+print("Epochs: ",epochs)
 pca = PCA(n_components=7)
 lib_components = pca.fit_transform(std_df)[:,0]
+
 data_components = std_df.dot(FPC)
 if lib_components[0]*data_components[0] < 0:
     data_components = data_components*-1
@@ -47,5 +48,9 @@ plt.xticks(rotation=90)
 plt.title("Using Library's First Principal Component")
 sorted = df_3.sort_values(by='PCA1', ascending=True)
 sns.barplot(x='Pais', y='PCA1', data=sorted)
-
 plt.show()
+
+
+
+
+
